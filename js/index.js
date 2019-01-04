ba1e5b03f60c4d088eeedeafd52415467a2f6f7e
huiminwu0 <huiminwu24@gmail.com>
2019-01-04 03:55:04 -0500

    Finish draft 1

--- js/index.js
+++ js/index.js
@@ -55,6 +55,58 @@ $(document).ready(() => {
     const $CDWillowWind = $('.CDWillowWind');
     const $CDWillowCont = $('#CDWillowCont');
     const $CDWillowHouse = $('#CDWillowHouse');
+    const $choiceCDRCross = $('.choiceCDRCross');
+    const $CDRCrossChicken =  $('.CDRCrossChicken');
+    const $CDRCrossEat = $('#CDRCrossEat');
+    const $CDRCrossPet = $('#CDRCrossPet');
+    const $choiceCDWCont = $('.choiceCDWCont');
+    const $CDWArrive = $('.CDWArrive');
+    const $CDWContN = $('#CDWContN');
+    const $CDWContY = $('#CDWContY');
+    const $choiceCDWHouse = $('.choiceCDWHouse');
+    const $CDWBagel = $('.CDWBagel');
+    const $CDWBagelN = $('#CDWBagelN');
+    const $CDWBagelY = $('#CDWBagelY');
+    const $choicePPQRun = $('.choicePPQRun');
+    const $choicePPQAttack = $('.choicePPQAttack');
+    const $PPQACandy = $('.PPQACandy');
+    const $PPQAttackY = $('#PPQAttackY');
+    const $PPQAttackN = $('#PPQAttackN');
+    const $choicePPRLeave = $('.choicePPRLeave');
+    const $PPRLPhoenix = $('.PPRLPhoenix');
+    const $PPRLeaveLeave = $('#PPRLeaveLeave');
+    const $PPRLeaveAttack = $('#PPRLeaveAttack');
+    const $choicePPRUnlock = $('.choicePPRUnlock');
+    const $PPRUZombies = $('.PPRUZombies');
+    const $PPRUnlockY = $('#PPRUnlockY');
+    const $PPRUnlockN = $('#PPRUnlockN');
+    const $choiceCDRCrossEat = $('.choiceCDRCrossEat');
+    const $choiceCDRCrossPet = $('.choiceCDRCrossPet');
+    const $CDRCBoss = $('.CDRCBoss');
+    const $Cfight = $('.Cfight');
+    const $run = $('.run');
+    const $choiceCDWContN = $('.choiceCDWContN');
+    const $CDWCNBoss = $('.CDWCNBoss');
+    const $choiceCDWContY = $('.choiceCDWContY');
+    const $choiceCDWBagelN = $('.choiceCDWBagelN');
+    const $CDWBNBoss = $('.CDWBNBoss');
+    const $choiceCDWBagelY = $('.choiceCDWBagelY');
+    const $choicePPQAttackY = $('.choicePPQAttackY');
+    const $choicePPQAttackN = $('.choicePPQAttackN');
+    const $PPQANBoss = $('.PPQANBoss');
+    const $Pfight = $('.Pfight');
+    const $choicePPRLeaveLeave = $('.choicePPRLeaveLeave');
+    const $choicePPRLeaveAttack = $('.choicePPRLeaveAttack');
+    const $choicePPRUnlockY = $('.choicePPRUnlockY');
+    const $PPRUYBoss = $('.PPRUYBoss');
+    const $PPRfight = $('#PPRfight');
+    const $choicePPRUnlockN = $('.choicePPRUnlockN');
+    const $choiceCfight = $('.choiceCfight');
+    const $choicePfight = $('.choicePfight');
+    const $choicePPRfight = $('.choicePPRfight');
+    const $choicerun = $('.choicerun');
+
+
 
 
      /*These function hide and show different HTML elements when other elements are clicked. This allows the player to go down different paths.*/
@@ -82,6 +134,18 @@ $(document).ready(() => {
         $choiceN.hide();
         $choiceNC.hide();
         $choicePDDodge.hide();
+        $choicePPQRun.hide();
+        $choiceCDRCrossEat.hide();
+        $choiceCDWContY.hide();
+        $choiceCDWBagelY.hide();
+        $choicePPQAttackY.hide();
+        $choicePPRLeaveLeave.hide();
+        $choicePPRLeaveAttack.hide();
+        $choicePPRUnlockN.hide();
+        $choiceCfight.hide();
+        $choicePfight.hide();
+        $choicePPRfight.hide();
+        $choicerun.hide();
         $intro.show();
         $choice1.show();
     });
@@ -177,6 +241,174 @@ $(document).ready(() => {
         $CDWillowWind.show();
     }); 
 
+    $PDQuillRun.on('click', () => {
+        $choicePDQuill.hide();
+        $choiceiPDQRun.show();
+        $PDQuillZ.hide();
+    }); 
+
+    $PDQuillAttack.on('click', () => {
+        $choicePDQuill.hide();
+        $choicePPQAttack.show();
+        $PDQuillZ.hide();
+        $PPQACandy.show();
+    }); 
+
+    $PDRunLeave.on('click', () => {
+        $choicePDRun.hide();
+        $choicePPRLeave.show();
+        $PDRunChest.hide();
+        $PPRLPhoenix.show();
+    }); 
+
+    $PDRunUnlock.on('click', () => {
+        $choicePDRun.hide();
+        $choicePPRUnlock.show();
+        $PDRunChest.hide();
+        $PPRUZombies.show();
+    }); 
+
+    $CDRavineRelocate.on('click', () => {
+        $choiceCDRavine.hide();
+        $choiceCDWillow.show();
+        $CDRavineBridge.hide();
+        $CDWillowWind.show();
+    }); 
+
+    $CDRavineCross.on('click', () => {
+        $choiceCDRavine.hide();
+        $choiceCDRCross.show();
+        $CDRavineBridge.hide();
+        $CDRCrossChicken.show();
+    }); 
+
+    $CDWillowCont.on('click', () => {
+        $choiceCDWillow.hide();
+        $choiceCDWCont.show();
+        $CDWillowWind.hide();
+        $CDWArrive.show();
+    }); 
+
+    $CDWillowHouse.on('click', () => {
+        $choiceCDWillow.hide();
+        $choiceCDWHouse.show();
+        $CDWillowWind.hide();
+        $CDWBagel.show();
+    }); 
+
+    $CDRCrossEat.on('click', () => {
+        $choiceCDRCross.hide();
+        $choiceCDRCrossEat.show();
+        $CDRCrossChicken.hide();
+    }); 
+
+    $CDRCrossPet.on('click', () => {
+        $choiceCDRCross.hide();
+        $choiceCDRCrossPet.show();
+        $CDRCrossChicken.hide();
+        $CDRCPBoss.show();
+    }); 
+
+    $CDWContN.on('click', () => {
+        $choiceCDWCont.hide();
+        $choiceCDWContN.show();
+        $CDWArrive.hide();
+        $CDWCNBoss.show();
+    }); 
+
+    $CDWContY.on('click', () => {
+        $choiceCDWCont.hide();
+        $choiceCDWContY.show();
+        $CDWArrive.hide();
+    }); 
+
+    $CDWBagelN.on('click', () => {
+        $choiceCDWHouse.hide();
+        $choiceCDWBagelN.show();
+        $CDWBagel.hide();
+        $CDWBNBoss.show();
+    }); 
+
+    $CDWBagelY.on('click', () => {
+        $choiceCDWHouse.hide();
+        $choiceCDWBagelY.show();
+        $CDWBagel.hide();
+    }); 
+
+    $PPQAttackY.on('click', () => {
+        $choicePPQAttack.hide();
+        $choicePPQAttackY.show();
+        $PPQACandy.hide();
+    }); 
+
+    $PPQAttackN.on('click', () => {
+        $choicePPQAttack.hide();
+        $choicePPQAttackN.show();
+        $PPQACandy.hide();
+        $PPQANBoss.show();
+    }); 
+
+    $PPRLeaveLeave.on('click', () => {
+        $choicePPRLeave.hide();
+        $choicePPRLeaveLeave.show();
+        $PPRLPhoenix.hide();
+    }); 
+
+    $PPRLeaveAttack.on('click', () => {
+        $choicePPrLeave.hide();
+        $choicePPRLeaveAttack.show();
+        $PPRLPhoenix.hide();
+    }); 
+
+    $PPRUnlockY.on('click', () => {
+        $choicePPRUnlock.hide();
+        $choicePPRUnlockY.show();
+        $PPRUZombies.hide();
+        $PPRUYBoss.show();
+    }); 
+
+    $PPRUnlockN.on('click', () => {
+        $choicePPRUnlock.hide();
+        $choicePPRUnlock.show();
+        $PPRUZombies.hide();
+    }); 
+
+    $Cfight.on('click', () => {
+        alert("oyoy");
+        $choiceCDRCrossPet.hide();
+        $CDRCPBoss.hide();
+        $choiceCDWContN.hide();
+        $CDWCNBoss.hide();
+        $choiceCDWBagelN.hide();
+        $CDWBNBoss.hide();
+        $choiceCfight.show();
+    });
+
+    $Pfight.on('click', () => {
+        $choicePPQAttackN.hide();
+        $PPQANBoss.hide();
+        $choicePfight.show();
+    });
+
+    $PPRfight.on('click', () => {
+        $choicePPRUnlockY.hide();
+        $PPRUYBoss.hide();
+        $choicePPRfight.show();
+    });
+    
+    $run.on('click', () => {
+        $choicePPRUnlockY.hide();
+        $PPRUYBoss.hide();
+        $choicePPQAttackN.hide();
+        $PPAANBoss.hide();
+        $choiceCDWBagelN.hide();
+        $choiceCDWContN.hide();
+        $choiceCDRCrossPet.hide();
+        $CDRCPBoss.hide();
+        $CDWCNBoss.hide();
+        $CDWBNBoss.hide();
+        $choicerun.show();
+    });
 });
 /*I copied and pasted these commented-out things, and then filled them in so I could work faster.*/
 /* 
@@ -184,6 +416,7 @@ $(document).ready(() => {
         $.hide();
         $.show();
         $.hide();
+        $.show();
     }); 
 */ 
 //const $ = $('');
